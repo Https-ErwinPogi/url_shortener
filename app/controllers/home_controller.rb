3a8e932e1a5a2415974dd504a8d7f1e2a73d2d9d
KodaCamp <>
2022-11-24 19:14:43 +0800

    Done show, new actions and views in home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def new
+    @link = Link.new
+    @links = Link.all
+  end
+
+  def show
+    @link = Link.find(params[:id])
+    redirect_to @link.original_url, allow_other_host: true
+  end
+end
