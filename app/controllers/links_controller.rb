ed0f74b20ace60f2bccc42ee3ea5592e292515e1
KodaCamp <>
2022-11-25 03:35:09 +0800

    done ip address and user agent req at create action add to view

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,8 +7,19 @@ class LinksController < ApplicationController
   def create
     shortener = Shortener.new(link_params[:original_url])
     @link = shortener.generate_short_link
+    @link.user = current_user
+    if Rails.env.development?
+      @link.ip_address = Net::HTTP.get(URI.parse('http://checkip.amazonaws.com/')).squish
+       @link.user_agent = request.env["HTTP_USER_AGENT"]
+    else
+      @link.ip_address = request.remote_ip
+      @link.user_agent = request.user_agent
+    end
     if @link.save
+      flash[:notice] = "Short Url was created successfully."
       redirect_back(fallback_location: root_path)
+    else
+      not_found
     end
   end
 
